976898ff124e8a912e6114dd59b2d3a71d2a6844
newsprime <124551400+newsprime@users.noreply.github.com>
2023-02-06 11:13:57 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <Header title="Next.js Toolbox" />
         <hr />
         <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
+          Here's an example of a Netlify Form! When you fill this out the
           submissions can be found in the Netlify Admin site.
         </p>
         <FeedbackForm />
