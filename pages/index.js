fbbf6c3f293b4f27c86fba06a7abf0c073889625
newsprime <124551400+newsprime@users.noreply.github.com>
2023-02-06 11:18:59 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,9 @@ export default function Home() {
         <p className="description">
           Here's an example of a Netlify Form! When you fill this out, the
           submissions can be found in the Netlify Admin site.
+    
+    Many people nowadays prefer to use news aggregator apps on their mobile devices over watching TV news channels or reading newspapers when on their commutes, during leisure time, or while eating. People choose to read the complete news as soon as possible, choose their own category of news, or just glance at the trending headlines rather than reading the entire news bulletin, so the reasons vary based on the individual. 
+
         </p>
         <FeedbackForm />
         <JokeBlock />
